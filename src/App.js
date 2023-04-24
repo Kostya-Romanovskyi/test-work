45a0ee7d8cb0f8dc6784eadedf87454b21734748
Kostya Romanovskyi <romanovskiyk97@gmail.com>
2023-04-24 17:34:10 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Home from "./pages/HomePage";
 import TweetsPage from "./pages/TweetsPage";
 import { Container } from "./App.styled";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Container>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="*" element={<Home />} /> */}
         <Route path="/tweets" element={<TweetsPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Container>
   );
